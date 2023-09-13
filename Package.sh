eed33ba1085f3eed4dac2ca60d615a45b0e06ae2
yogitachourey123 <111501961+yogitachourey123@users.noreply.github.com>
2023-09-13 16:04:21 +0530

    Update Package.sh

--- Package.sh
+++ Package.sh
@@ -1,13 +1,11 @@
 function InstallPkg() {
- local packageName="$1"
+local packageName="$1"
 # Check if the package is already installed
- if dpkg -l | grep -q "ii $package_name";
-   then echo "The $package_name package is already installed."
- else
-# Update package lists
-   apt update
- #Install the provided package
-   apt install $packageName
- fi
+if [ dpkg -s "$packageName" >/dev/null 2>&1 && ]; then
+    echo "$packageName is already installed."
+else
+#Install the provided package
+  sudo apt-get install $packageName -
+fi
 }
 InstallPkg "$1"
